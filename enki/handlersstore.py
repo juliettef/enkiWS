781ccd42d919dee71c915ffb43e349ea2b44e3f7
juliettef <juliette@enkisoftware.com>
2016-01-28 15:41:28 +0000

    [ C081 ] FastSpring integration - in progress

--- enki/handlersstore.py
+++ enki/handlersstore.py
@@ -10,8 +10,8 @@ from enki.extensions import Extension
 from enki.extensions import ExtensionPage
 
 
-products = { 'avoyd': { 'name' : 'Avoyd', 'price' : 3.00 },
-             'avoyd-1999' : { 'name' : 'Avoyd-1999', 'price' : 0.00 },
+products = { 'avoyd': { 'displayname' : 'Avoyd', 'price' : 3.00 },
+             'avoyd-1999' : { 'displayname' : 'Avoyd-1999', 'price' : 0.00 },
              }
 
 
@@ -39,7 +39,7 @@ class HandlerStore( enki.HandlerBase ):
 		                            purchase_price = float( products[ product ][ 'price' ]),
 		                            )
 		EnkiModelProductKey.put( item )
-		product_name = products[ product ][ 'name' ]
+		product_name = products[ product ][ 'displayname' ]
 		product_code_formatted = enki.libstore.insert_dash_5_10( product_code.encode( 'utf-8' ) )
 		link_product_code = enki.libutil.get_local_url( 'profile', { 'code' : product_code_formatted })
 		if purchaser_email: # TODO: get email address
@@ -55,10 +55,25 @@ class HandlerStore( enki.HandlerBase ):
 		                  CSRFtoken = self.create_CSRF( 'store' ),
 		                  product = products[ product ])
 
-#TODO: 1. Add POST render of success page.
+#TODO: 1. [DONE] Add POST render of success page.
 #TODO: 2. Add a page extension to the profile (not public) which lists your products you have purchased, and their status (registered or not).
 #TODO: 3. Add a way to register a product you own.
 #TODO: 4. Add a way to register any product from a key.
+
+
+class HandlerGenLicenseFastSpring( enki.HandlerBase ):
+
+	def post( self ):
+		quantity = self.request.get( 'quantity' )
+		product_codes = ''
+		if quantity:
+			quantity = int( quantity )
+			while quantity > 0:
+				product_codes += enki.libstore.insert_dash_5_10( enki.libstore.generate_product_code()) + '\n'
+				quantity -= 1
+			self.response.write( product_codes )
+			return
+		self.abort( 404 )
 
 
 class ExtensionPageProducts( ExtensionPage ):
@@ -71,13 +86,27 @@ class ExtensionPageProducts( ExtensionPage ):
 		return data
 
 
+class ExtensionPageLibrary( ExtensionPage ):
+
+	def __init__( self ):
+		ExtensionPage.__init__( self, route_name = 'profile', template_include = 'inclibrary.html' )
+
+	def get_data( self, handler ):
+		data = []
+#		user  = self.enki_user.key.id()
+		user_id = 6173208034148352
+		data = enki.libstore.fetch_EnkiProuctKey_by_purchaser( user_id )
+		return data
+
+
 class ExtensionStore( Extension ):
 
 	def get_routes( self ):
-		return  [ webapp2.Route( '/store', HandlerStore, name = 'store' )]
+		return  [ webapp2.Route( '/store', HandlerStore, name = 'store' ),
+		          webapp2.Route( '/genlicensefastspring', HandlerGenLicenseFastSpring, name = 'genlicensefastspring'  )]
 
 	def get_navbar_items( self ):
 		return [( enki.libutil.get_local_url( 'store' ), 'store', _( "Store" ))]
 
 	def get_page_extensions( self ):
-		return [ ExtensionPageProducts()]
+		return [ ExtensionPageProducts(), ExtensionPageLibrary()]
--- enki/libstore.py
+++ enki/libstore.py
@@ -20,3 +20,12 @@ def insert_dash_5_10( string ):
 
 #=== QUERIES ==================================================================
 
+
+def fetch_EnkiProuctKey_by_purchaser( user_id ):
+	list = EnkiModelProductKey.query( EnkiModelProductKey.purchaser_user_id == user_id ).fetch()
+	return list
+
+
+def fetch_EnkiProuctKey_by_registered_to( user_id ):
+	list = EnkiModelProductKey.query( EnkiModelProductKey.registered_to_user == user_id ).fetch()
+	return list
