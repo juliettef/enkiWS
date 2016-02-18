174a12c3bb43728906196366868554a6831ecb62
juliettef <juliette@enkisoftware.com>
2016-02-18 09:55:16 +0000

    Display name lowercase property added

--- enki/modeldisplayname.py
+++ enki/modeldisplayname.py
@@ -5,6 +5,7 @@ class EnkiModelDisplayName( model.Model ):
 
 	user_id = model.IntegerProperty()
 	prefix = model.StringProperty() # prefix e.g. 'Jane'
+	prefix_lower = model.ComputedProperty(lambda self: self.prefix.lower()) # lowercase prefix e.g. "jane"
 	suffix = model.StringProperty() # suffix e.g. '#1234' => full display name = 'Jane#1234'
 	current = model.BooleanProperty( default = True )
 	time_created = model.DateTimeProperty( auto_now_add = True )
