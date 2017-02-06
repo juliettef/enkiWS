84d3477cf32f2d19a67a64b5e38810016cf1f044
juliettef <juliette@enkisoftware.com>
2017-02-06 17:43:27 +0000

    [ C222 ] fix wording

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 import os
 
-answer = raw_input( 'Upload enkiDL Y/N? ').upper()
+answer = raw_input( 'Upload Y/N? ').upper()
 if answer == 'Y':
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set app/num_file_upload_processes 1' )	# Issue 1036 workaround https://code.google.com/p/google-cloud-sdk/issues/detail?id=1036
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set project enkisoftware-webservices' )
