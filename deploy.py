f7d660dd8b6f189f0b3a9da9b56f58052a647432
juliettef <juliette@enkisoftware.com>
2017-04-10 14:34:51 +0100

    [ C264 ] exclude some directories and files from deployment rename script upload.py to deploy.py Removed workaround for gcloud issue 1036 fixed https://issuetracker.google.com/issues/35907209

--- deploy.py
+++ deploy.py
@@ -2,7 +2,6 @@ import os
 
 version = raw_input( 'Upload ? version nr. or N ')
 if version.isdigit():
-	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set app/num_file_upload_processes 1' )	# Issue 1036 workaround https://code.google.com/p/google-cloud-sdk/issues/detail?id=1036
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set project enkisoftware-webservices' )
 	# os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app create --region=us-central')
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app deploy app.yaml dos.yaml index.yaml cron.yaml --version={v} --promote -q'.format( v = version ))
