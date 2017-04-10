7ec100ff9aeaaa0a08b5bb952c81e8892ed3cf9a
juliettef <juliette@enkisoftware.com>
2017-04-10 16:12:29 +0100

    [ C264 ] remove --promote

--- deploy.py
+++ deploy.py
@@ -4,6 +4,6 @@ version = raw_input( 'Upload ? version nr. or N ')
 if version.isdigit():
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set project enkisoftware-webservices' )
 	# os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app create --region=us-central')
-	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app deploy app.yaml dos.yaml index.yaml cron.yaml --version={v} --promote -q'.format( v = version ))
+	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app deploy app.yaml dos.yaml index.yaml cron.yaml --version={v} -q'.format( v = version ))
 else:
 	print 'Aborted'
