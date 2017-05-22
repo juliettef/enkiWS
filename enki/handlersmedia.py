00ecb9329adfa500ee1ba6d533eb926ba0889aeb
juliettef <juliette@enkisoftware.com>
2017-05-22 17:17:20 +0100

    [ C276 ] Media page json data and handler for video galleries

--- enki/handlersmedia.py
+++ enki/handlersmedia.py
@@ -48,56 +48,103 @@ class HandlerMedia( enki.HandlerBase ):
 
 	@classmethod
 	def create_media_page( cls, media_json ):
-		# galleries
-		NUM_COLUMNS_DEFAULT = 3
-		HTML_GALLERY_TITLE = '''\t\t\t<h3>{gallery_title}</h3>\n'''
+		# image and video galleries
+		HTML_HEADER = '''\n\t\t\t<h2>{header}</h2>\n'''
+		HTML_GALLERY_TITLE = '''\n\t\t\t<h3>{gallery_title}</h3>\n'''
 		HTML_GALLERY_DESCRIPTION = '''\t\t\t<h4>{gallery_description}</h4>\n'''
-		HTML_IMAGES_ROW_START = '''\t\t\t<div class="row">\n'''
-		HTML_IMAGES_ROW_END = '''\t\t\t</div>\n'''
-		HTML_IMAGE = '''\t\t\t\t<div class="col-xs-{col_width}">\n''' + '''\t\t\t\t\t<p><a href="{url}">\n''' + \
-		'''\t\t\t\t\t<img alt="{img_alt}" src="{img_src}" width=100% class="img-rounded"></a></p>\n''' + '''\t\t\t\t</div>\n'''
-		HTML_IMAGE_NONE = '''\t\t\t\t<div class="col-xs-{col_width}">\n''' + '''\t\t\t\t</div>\n'''
+		HTML_ROW_START = '''\t\t\t<div class="row">\n'''
+		HTML_ROW_END = '''\t\t\t</div>\n'''
+		HTML_COL_START = '''\t\t\t\t<div class="col-xs-{col_width}">\n'''
+		HTML_COL_END = '''\t\t\t\t</div>\n'''
+		HTML_COL_EMPTY = HTML_COL_START + HTML_COL_END
+		HTML_IMAGE = HTML_COL_START + \
+					 '''\t\t\t\t\t<p><a href="{url}">\n\t\t\t\t\t<img alt="{img_alt}" src="{img_src}" width=100% class="img-rounded"></a></p>\n''' + \
+					 HTML_COL_END
+		HTML_VIDEO = HTML_COL_START + \
+					 '''\t\t\t\t\t<div class="embed-responsive embed-responsive-16by9">\n\t\t\t\t\t\t<iframe src="{url}" frameborder="0" allowfullscreen></iframe>\n\t\t\t\t\t</div>\n''' + \
+					 HTML_COL_END
 		media_html = ''
-		galleries = media_json[ 'galleries' ]
-		if galleries:
+		media = media_json[ 'media' ]
+		galleries_of_images = media[ 'galleries_of_images' ]
+		if galleries_of_images:
+			media_html += HTML_HEADER.format( header = media[ 'HEADER_IMAGES' ]) if media[ 'HEADER_IMAGES' ] else ''
 			iter_gallery = 0
-			for gallery in galleries:
+			for gallery in galleries_of_images:
 				# set gallery title and description (if exist)
 				media_html += HTML_GALLERY_TITLE.format( gallery_title = gallery[ 'title' ]) if gallery[ 'title' ] else ''
 				media_html += HTML_GALLERY_DESCRIPTION.format( gallery_description = gallery[ 'description' ]) if gallery[ 'description' ] else ''
 				# retrieve and set gallery images
 				images = gallery[ 'images' ]
 				if images:
-					num_columns = int( gallery[ 'num_columns' ]) if gallery[ 'num_columns' ] else NUM_COLUMNS_DEFAULT
+					num_columns = int( gallery[ 'num_columns_override' ]) if gallery[ 'num_columns_override' ] else int( media[ 'NUM_COLUMNS_IMAGES_DEFAULT' ])
 					column_width = 12 // num_columns # ensure the number of columns requested fit in the bootsrap grid https://getbootstrap.com/css/#grid
-					num_images = len(images)
+					num_images = len( images )
 					num_rows = num_images // num_columns + ( num_images % num_columns > 0 )	# round up
 					iter_image = 0
 					iter_row = 0
 					while iter_row < num_rows:
 						# start a new row
-						media_html += HTML_IMAGES_ROW_START
+						media_html += HTML_ROW_START
 						iter_column = 0
 						while iter_column < num_columns:
 							if iter_image < num_images:
 								# cell contains an image
 								image = images[ iter_image ]
 								url = enki.libutil.get_local_url( 'media', { 'g' : str( iter_gallery ), 'i' : str( iter_image )})
-								media_html += HTML_IMAGE.format( col_width = column_width, url=url, img_alt = image[ 'img_alt' ], img_src = image[ 'img_src' ]) # gallery_num = iter_gallery, image_num = iter_image,
+								media_html += HTML_IMAGE.format( col_width = column_width, url=url, img_alt = image[ 'img_alt' ], img_src = image[ 'img_src' ])
 								iter_image += 1
 							else:
 								# blank cell
-								media_html += HTML_IMAGE_NONE.format( col_width = column_width )
+								media_html += HTML_COL_EMPTY.format( col_width = column_width )
 							iter_column += 1
 						# end row
-						media_html += HTML_IMAGES_ROW_END
+						media_html += HTML_ROW_END
 						iter_row += 1
 				else:
 					media_html += '''<p class="text-info">No images available.</p>'''
 				# increment gallery index
 				iter_gallery += 1
 		else:
-			media_html = '''<p class="text-info">No galleries available.</p>'''
+			media_html = '''<p class="text-info">No galleries of images available.</p>'''
+
+		galleries_of_videos = media[ 'galleries_of_videos' ]
+		if galleries_of_videos:
+			media_html += HTML_HEADER.format(header = media[ 'HEADER_VIDEOS' ]) if media[ 'HEADER_VIDEOS' ] else ''
+			for gallery in galleries_of_videos:
+				# set gallery title and description (if exist)
+				media_html += HTML_GALLERY_TITLE.format( gallery_title = gallery[ 'title' ]) if gallery[ 'title' ] else ''
+				media_html += HTML_GALLERY_DESCRIPTION.format( gallery_description = gallery[ 'description' ]) if gallery[ 'description' ] else ''
+				# retrieve and set gallery videos
+				videos = gallery[ 'videos' ]
+				if videos:
+					num_columns = int( gallery[ 'num_columns_override' ]) if gallery[ 'num_columns_override' ] else int( media[ 'NUM_COLUMNS_VIDEOS_DEFAULT' ])
+					column_width = 12 // num_columns # ensure the number of columns requested fit in the bootsrap grid https://getbootstrap.com/css/#grid
+					num_videos = len( videos )
+					num_rows = num_videos // num_columns + ( num_videos % num_columns > 0 )    # round up
+					iter_video = 0
+					iter_row = 0
+					while iter_row < num_rows:
+						# start a new row
+						media_html += HTML_ROW_START
+						iter_column = 0
+						while iter_column < num_columns:
+							if iter_video < num_videos:
+								# cell contains an image
+								video = videos[ iter_video ]
+								media_html += HTML_VIDEO.format( col_width = column_width, url = video[ 'iframe_src' ])
+								iter_video += 1
+							else:
+								# blank cell
+								media_html += HTML_COL_EMPTY.format( col_width = column_width )
+							iter_column += 1
+						# end row
+						media_html += HTML_ROW_END
+						iter_row += 1
+				else:
+					media_html += '''<p class="text-info">No videos available.</p>'''
+		else:
+			media_html += '''<p class="text-info">No galleries of videos available.</p>'''
+
 		return media_html
 
 
