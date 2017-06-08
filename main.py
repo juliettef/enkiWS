aeaf66fd7a35cbf528ad3b3b7cd9f197a48132b3
juliettef <juliette@enkisoftware.com>
2017-06-08 11:45:07 +0100

    [ C121 ] Custom 404

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import enki
 import enki.libutil
 import enki.textmessages as MSG
 
+
 class HandlerMain( enki.HandlerBase ):
 
 	def get(self):
@@ -14,6 +15,13 @@ class HandlerMain( enki.HandlerBase ):
 			self.add_infomessage( MSG.WARNING(), 'Setup incomplete, see <a class="alert-link"" href="https://github.com/juliettef/enkiWS#enabling-oauth-login-with-google-facebook-twitter">documentation</a>.')
 		self.render_tmpl( 'home.html', False,
 		                  active_menu = 'home' )
+
+
+class HandlerCustom404( enki.HandlerBase ):
+
+	def get( self, path ):
+		self.render_tmpl( 'notfound.html', permanent = True )
+
 
 enki.ExtensionLibrary.set_extensions([ enki.ExtensionStore(),
                                        enki.ExtensionForums(),
@@ -33,5 +41,7 @@ routes += enki.routes_account \
 routes_copy = copy.deepcopy( routes )
 locale_routes = [ webapp2_extras.routes.PathPrefixRoute('/<locale:[a-z]{2}_[A-Z]{2}>',  [ webapp2_extras.routes.NamePrefixRoute('locale-', routes ) ] ) ]
 locale_routes += routes_copy + settings.get_routes_oauth()
+locale_routes.append( webapp2.Route( '<:.*>', HandlerCustom404, name = 'custom_404' ))
+
 
 app = webapp2.WSGIApplication( routes = locale_routes, debug = enki.libutil.is_debug(), config = settings.config )
