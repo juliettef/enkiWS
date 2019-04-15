764b8d147401156e05ef2acd696f0561fee32b6c
juliettef <juliette@enkisoftware.com>
2019-04-15 19:27:01 +0100

    Removed deprecated dos.yaml

--- deploy.py
+++ deploy.py
@@ -4,6 +4,6 @@ version = raw_input( 'Upload ? version nr. or N ')
 if version.isdigit():
 	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" config set project enkiws' )
 	# os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app create --region=europe-west')
-	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app deploy app.yaml dos.yaml index.yaml cron.yaml --version={v} -q'.format( v = version ))
+	os.system( r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\gcloud" app deploy app.yaml index.yaml cron.yaml --version={v} -q'.format( v = version ))
 else:
 	print 'Aborted'
